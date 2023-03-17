8625f60c1a285a84b30da952e42e06160524bbc6
captaincook-del <cpataincook-del@protonmail.com>
2023-03-17 19:47:21 +0100

    add ssl support

--- exporter.py
+++ exporter.py
@@ -2,31 +2,38 @@
 
 """ Data Exporter"""
 
-from requests import post, Session
+from requests import Session
+from requests.exceptions import SSLError
 from json import dumps
 
 class DataExporter:
     """ class Data Exporter """
     def __init__(self, kwargs, logger) -> None:
         self.logger = logger
+        self.session = Session()
         self.url = kwargs.get('host') + ":" + str(kwargs.get('port')) + kwargs.get('url', '')
-        self.headers = {'Content-Type': 'application/json'}
-        self.verify = kwargs.get('verify', False)
+        self.session.headers.update({'Content-Type': 'application/json'})
+
+        if kwargs.get('verify', False):
+            self.session.verify = kwargs.get('verify')
+        if kwargs.get('cert', False):
+            self.session.cert = kwargs.get('cert')
 
     def send_data(self, data):
         """ Send data to the server """
         try:
-            response = post(
+            response = self.session.post(
                 self.url,
                 data=dumps(data),
-                headers=self.headers,
-                verify=self.verify
             )
             if response.status_code != 200:
                 print('Error: ' + response.text)
                 self.logger.warning('Warning: ' + response.text)
             else:
                 self.logger.info('Data sent')
+        except SSLError as e:
+            print('Error SSL: ' + str(e))
+            self.logger.error('Error: ' + str(e))
         except Exception as e:
             print('Error: ' + str(e))
             self.logger.error('Error: ' + str(e))
